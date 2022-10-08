565c57cc33ded9bec31769c1e2784750ef7f1efe
Oleg Kiselev <root@e5ash.com>
2022-10-09 02:27:18 +0300

    Init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  components: [
+    { path: '~/components', extensions: ['vue'] }
+  ],
+  alias: {
+    "~styles": "/assets/styles",
+    "~images": "/assets/images",
+  },
+  css: [
+    '@/assets/styles/main.scss'
+  ],
 })
