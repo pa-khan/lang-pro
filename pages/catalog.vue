b1c8f99b87f425e6151ffdb863e8a744ea966c59
Oleg Kiselev <root@e5ash.com>
2022-10-11 00:36:17 +0300

    Desktop

---
+++ pages/catalog.vue
@@ -0,0 +1,17 @@
+<template>
+  <Breadcrumb />
+  <Title value="Курсы" />
+  <Cat />
+</template>
+<script>
+export default {
+  head: {
+    title: 'Курсы'
+  },
+  setup() {
+    definePageMeta({
+      title: 'Курсы'
+    });
+  }
+}
+</script>
